3995d88bcb90a6409a09f5796a29d3dfd4a154b5
Daniel Balparda <balparda@gmail.com>
2025-07-20 18:51:52 +0100

    GCD

--- src/transcrypto/transcrypto.py
+++ src/transcrypto/transcrypto.py
@@ -12,3 +12,37 @@ __version__: tuple[int, int, int] = (1, 0, 0)  # v1.0.0, 2025-07-20
 
 class Error(Exception):
   """TransCrypto exception."""
+
+
+def GCD(a: int, b: int) -> int:
+  """Greatest Common Divisor, by the Euclid method."""
+  if a < 0 or b < 0:
+    raise Error(f'negative input: {a=} , {b=}')
+  if a < b:
+    a, b = b, a
+  while b:
+    r = a % b
+    a, b = b, r
+  return a
+
+
+def ExtendedGCD(a: int, b: int) -> tuple[int, int, int]:
+  """Greatest Common Divisor, extended, by the Euclid method.
+
+  Returns:
+    (gcd, x, y) so that a * x + b * y = gcd
+  """
+  if a < 0 or b < 0:
+    raise Error(f'negative input: {a=} , {b=}')
+  swapped = False
+  if a < b:
+    a, b = b, a
+    swapped = True
+  if not b:
+    return (a, 0 if swapped else 1, 1 if swapped else 0)
+  x1, x2, y1, y2 = 0, 1, 1, 0
+  while b:
+    q, r = divmod(a, b)
+    x, y = x2 - q * x1, y2 - q * y1
+    a, b, x1, x2, y1, y2 = b, r, x, x1, y, y1
+  return (a, y2 if swapped else x2, x2 if swapped else y2)
--- tests/transcrypto_test.py
+++ tests/transcrypto_test.py
@@ -17,6 +17,40 @@ __author__ = 'balparda@github.com (Daniel Balparda)'
 __version__: tuple[int, int, int] = transcrypto.__version__  # tests inherit version from module
 
 
+@pytest.mark.parametrize('a, b, gcd, x, y', [
+    (0, 0, 0, 1, 0),
+    (0, 1, 1, 0, 1),
+    (1, 0, 1, 1, 0),
+    (1, 2, 1, 1, 0),
+    (2, 1, 1, 0, 1),
+    (12, 18, 6, -1, 1),
+    (3, 7, 1, -2, 1),
+    (7, 3, 1, 1, -2),
+    (100, 24, 4, 1, -4),
+    (24, 100, 4, -4, 1),
+    (367613542, 2136213, 59, 15377, -2646175),
+    (2354153438, 65246322, 2, 4133449, -149139030),
+    (7238649876345, 36193249381725, 7238649876345, 1, 0),
+])
+def test_GCD(a: int, b: int, gcd: int, x: int, y: int) -> None:
+  """Test."""
+  assert transcrypto.GCD(a, b) == gcd
+  assert transcrypto.ExtendedGCD(a, b) == (gcd, x, y)
+  assert gcd == a * x + b * y
+
+
+@pytest.mark.parametrize('a, b', [
+    (-1, 1),
+    (1, -1),
+])
+def test_GCD_negative(a, b) -> None:
+  """Test."""
+  with pytest.raises(transcrypto.Error, match='negative input'):
+    transcrypto.GCD(a, b)
+  with pytest.raises(transcrypto.Error, match='negative input'):
+    transcrypto.ExtendedGCD(a, b)
+
+
 if __name__ == '__main__':
   # run only the tests in THIS file but pass through any extra CLI flags
   args: list[str] = sys.argv[1:] + [__file__]
